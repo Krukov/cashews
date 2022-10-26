437c235edfaeb6e90be404f3152259ed32b74a80
Krukov D <Glebov.ru@gmail.com>
2022-10-26 22:40:51 +0300

    Update cashews/_typing.py
    Co-authored-by: Aleksei Maslakov <unmade@users.noreply.github.com>

--- cashews/_typing.py
+++ cashews/_typing.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from typing import Any, Callable, Dict, Optional, Tuple, Union
 
-TTL = Union[int, str, timedelta, Callable[[Any], Union[int, str, timedelta, None]]]
+_TTLTypes = Union[int, float, str, timedelta, None]
+TTL = Union[TTLTypes, Callable[[Any], _TTLTypes]]
 CallableCacheCondition = Callable[[Any, Tuple, Dict, Optional[str]], bool]
 CacheCondition = Union[CallableCacheCondition, str, None]
